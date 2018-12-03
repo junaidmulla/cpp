d5e873f6a81f844eeda9ee6707f88ea0486fe9d2
junaidmulla <45563634+junaidmulla@users.noreply.github.com>
2018-12-03 18:40:34 +0530

    Simple C++ Program to print hello world

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+/*
+ * Filename: git.cpp
+ * Project: Simple C++ Program to print hello world
+ * Created Date: Monday, December 3rd 2018, 6:28:53 pm
+ * Author: Junaid Mulla
+ * 
+ * Revision History : 
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World";
+    return 0;
+}
